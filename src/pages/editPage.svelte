f77aadeefa166d1ecfd9ee0bfd14290a6a055474
Aditya Ghidora <aditya@ghidora.tech>
2023-01-07 19:40:50 +0530

    some basic editing working now

--- src/pages/editPage.svelte
+++ src/pages/editPage.svelte
@@ -1,1 +1,68 @@
-<div>edit</div>
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { fabric } from "fabric";
+    let canvas;
+    let canvasColor = "#032312";
+    onMount(() => {
+        canvas = new fabric.Canvas("canvas", {
+            backgroundColor: canvasColor,
+            snapAngle: 0,
+        });
+    });
+    function addRect() {
+        let rect = new fabric.Rect({
+            left: 150,
+            top: 150,
+            fill: "blue",
+            width: 200,
+            height: 200,
+        });
+        canvas.add(rect);
+    }
+    function save() {
+        console.log(canvas.toSVG());
+    }
+    function addText() {
+        let text = new fabric.Text("Hello There", { left: 100, right: 100 });
+        canvas.add(text);
+    }
+    function changeCanvasColor() {
+        canvas.setBackgroundColor(canvasColor, canvas.renderAll.bind(canvas));
+    }
+    $: console.log(canvasColor);
+</script>
+
+<section>
+    <div class="toolbar">
+        <button on:click={addRect}>Add Rect</button>
+        <button on:click={addText}>Add Text</button>
+        <button on:click={save}>Save to json</button>
+        <div id="color-picker">
+            <input type="color" name="bg-color" bind:value={canvasColor} on:change={changeCanvasColor} />
+            <label for="background">Canvas background color</label>
+        </div>
+    </div>
+    <div class="canvas-container">
+        <canvas id="canvas" width="1050" height="600" />
+    </div>
+</section>
+
+<style>
+    section {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 92vh;
+    }
+
+    #canvas {
+        background-color: white;
+        border-radius: 3px;
+        margin: 1rem 2rem;
+    }
+    .toolbar {
+        position: absolute;
+        top: 10vh;
+        left: 10vh;
+    }
+</style>
