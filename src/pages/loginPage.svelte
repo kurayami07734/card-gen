5522d65d7d107fde88508897cef85930a4a15ebd
Aditya Ghidora <aditya@ghidora.tech>
2023-01-10 11:56:27 +0530

    added facebook login

--- src/pages/loginPage.svelte
+++ src/pages/loginPage.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
     import { signInWithPopup } from "firebase/auth";
-    import { auth, googleProvider } from "../firebase";
-    function login() {
+    import { auth, googleProvider, facebookProvider } from "../firebase";
+    function loginGoogle() {
         signInWithPopup(auth, googleProvider).then((res) => (user = res.user));
         console.log(user);
+    }
+    function loginFacebook() {
+        signInWithPopup(auth, facebookProvider).then((res) => (user = res.user));
+        console.log(user);
+    }
+    function logout() {
+        auth.signOut();
+        user = null;
     }
     export let user;
 </script>
 
 <section>
+    {#if user}
+        <p>{user.displayName}</p>
+        <button on:click={logout}>Logout</button>
+    {:else}
     <div class="login-box">
-        <button on:click={login}>Sign in with google</button>
+        <button on:click={loginGoogle}>Sign in with google</button>
+        <button on:click={loginFacebook}>Sign in with facebook</button>
     </div>
+    {/if}
+    
 </section>
