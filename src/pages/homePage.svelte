5cbd455f77320dab2d8962e00e6f00fa8b48471e
Aditya Ghidora <aditya@ghidora.tech>
2023-01-11 10:27:32 +0530

    simplified the homepage

--- src/pages/homePage.svelte
+++ src/pages/homePage.svelte
@@ -2,70 +2,59 @@
     let value = "";
 </script>
 
-<div class="background">
-    <section>
-        <div class="hero">
-            <h1>Business Cards</h1>
-            <h3>Make a business card in minutes</h3>
-        </div>
-        <div class="cta">
-            <input type="search" bind:value placeholder="Enter a business name" />
-            <button>Go</button>
-        </div>
-    </section>
-</div>
+<div class="background" />
+<section>
+    <div class="hero">
+        <h1>Make a business card in minutes</h1>
+    </div>
+    <div class="cta">
+        <input type="search" bind:value placeholder="Enter a business name" />
+        <button>Go</button>
+    </div>
+</section>
 
 <style>
     section {
         display: flex;
         justify-content: center;
         flex-direction: column;
-        align-items: flex-start;
+        align-items: center;
+        z-index: 2;
         height: 91vh;
-        margin-left: 15vh;
     }
     h1 {
-        font-size: 40px;
-        line-height: 1.24rem;
-    }
-    h3 {
-        font-weight: 200;
+        font-size: 60px;
     }
     .background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        z-index: -1;
         background-image: url("../assets/dark-card-bg.jpg");
         background-size: cover;
+        filter: grayscale(1) brightness(0.65);
+    }
+    .cta {
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
     .cta > button {
         background-color: #0066ff;
-        width: 60vw;
+        padding: 1rem;
+        font-size: 1.5rem;
+        font-weight: 600;
+        max-width: 60vw;
     }
     .cta > input {
-        padding: 0.23rem;
-        padding-left: 1rem;
+        padding: 0.8rem;
+        /* padding-left: 1rem; */
         background-color: white;
         border-radius: 8px;
-        font-size: 1.3rem;
-        width: 60vw;
+        font-size: 1.5rem;
+        max-width: 60vw;
         color: black;
-    }
-    @media (min-width: 800px) {
-        .cta > input {
-            padding: 0.23rem;
-            padding-left: 1rem;
-            background-color: white;
-            border-top-left-radius: 8px;
-            border-bottom-left-radius: 8px;
-            font-size: 1.4rem;
-            width: 400px;
-            color: black;
-        }
-        .cta > button {
-            transform: translate(-2rem, -0.238rem);
-            z-index: 2;
-            background-color: #0066ff;
-            border-top-left-radius: 0;
-            width: auto;
-            border-bottom-left-radius: 0;
-        }
     }
 </style>
